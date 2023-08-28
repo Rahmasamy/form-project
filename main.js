fb8a1977f5377c7c7340720634d7a24816aef8b1
Rahma samy <rahmasamy949@gmail.com>
2023-08-28 22:16:39 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+const container = document.querySelector('.container');
+const loginLink = document.querySelector('.login-link');
+const regisrerLink = document.querySelector('.register-link');
+const btn = document.querySelector('.btn');
+const closex = document.querySelector('.close');
+
+regisrerLink.addEventListener('click', () => {
+    container.classList.add('active');
+});
+loginLink.addEventListener('click', () => {
+    container.classList.remove('active');
+});
+btn.addEventListener('click', () => {
+    container.classList.add('active-btn');
+});
+
+closex.addEventListener('click', () => {
+    container.classList.remove('active-btn');
+});
+function validate() {
+    var x = document.forms["form1"]["fname"].value;
+    var max_length = 20;
+    if (x == "" || x.length > max_length) {
+        alert("Incorrect input");
+        return false;
+    }
+
+}
+
+
